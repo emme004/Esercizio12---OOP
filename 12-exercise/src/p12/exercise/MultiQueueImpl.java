288473ffe9e50a8582a884b627664d0b7029aee2
emme004 <andrea.monti24@studio.unibo.it>
2024-11-01 18:15:38 +0100

    half part completed

--- 12-exercise/src/p12/exercise/MultiQueueImpl.java
+++ 12-exercise/src/p12/exercise/MultiQueueImpl.java
@@ -1,45 +1,66 @@
 package p12.exercise;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
+    private Map<Q, Queue<T>> map;
+
+    
+
+    public MultiQueueImpl(Map<Q, Queue<T>> map) {
+        map = new HashMap<>();
+    }
 
     @Override
     public Set<Q> availableQueues() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'availableQueues'");
+        return map.keySet();    
     }
 
     @Override
-    public void openNewQueue(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'openNewQueue'");
+    public void openNewQueue(Q queue) throws IllegalArgumentException {
+        if(map.containsKey(queue)){
+            throw new IllegalArgumentException("The queue is already available");
+        }
+        map.put(queue, new LinkedList<T>());
+    
     }
 
     @Override
-    public boolean isQueueEmpty(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isQueueEmpty'");
+    public boolean isQueueEmpty(Q queue) throws IllegalArgumentException{
+        if(!map.containsKey(queue)){
+            throw new IllegalArgumentException("The queue is not available");
+        }    
+        return map.get(queue).isEmpty();
     }
 
     @Override
-    public void enqueue(T elem, Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'enqueue'");
+    public void enqueue(T elem, Q queue) throws IllegalArgumentException{
+        if(!map.containsKey(queue)){
+            throw new IllegalArgumentException("The queue is not available");
+        } 
+        map.get(queue).add(elem);
     }
 
     @Override
-    public T dequeue(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dequeue'");
+    public T dequeue(Q queue) throws IllegalArgumentException {
+        if(!map.containsKey(queue)){
+            throw new IllegalArgumentException("The queue is not available");
+        } 
+        return map.get(queue).poll();
     }
 
     @Override
     public Map<Q, T> dequeueOneFromAllQueues() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dequeueOneFromAllQueues'");
+        Map<Q, T> dequeuedElement = new HashMap<>();
+        for(Q queue : map.keySet()){
+            dequeuedElement.put(queue, map.get(queue).poll());
+        }
+        return dequeuedElement;
     }
 
     @Override
