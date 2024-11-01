8d486ba06e9c6846390e5533feb3eb3adaf6d340
emme004 <andrea.monti24@studio.unibo.it>
2024-11-01 17:50:03 +0100

    push verification completed

--- 12-exercise/src/p12/exercise/MultiQueue.java
+++ 12-exercise/src/p12/exercise/MultiQueue.java
@@ -1,13 +1,6 @@
 package p12.exercise;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 
 /**
