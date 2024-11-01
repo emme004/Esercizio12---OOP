168655306e3ba467053ed331f904207f14ac8c90
emme004 <andrea.monti24@studio.unibo.it>
2024-11-01 17:48:18 +0100

    Insertion of import

--- 12-exercise/src/p12/exercise/MultiQueue.java
+++ 12-exercise/src/p12/exercise/MultiQueue.java
@@ -1,6 +1,13 @@
 package p12.exercise;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 
 /**
