46ebaecce0d3a47b0ba0eb1f608114568e757e92
emme004 <andrea.monti24@studio.unibo.it>
2024-11-02 16:15:40 +0100

    All it's ok

--- 12-exercise/src/p12/exercise/MultiQueueImpl.java
+++ 12-exercise/src/p12/exercise/MultiQueueImpl.java
@@ -1,6 +1,7 @@
 package p12.exercise;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -8,11 +9,9 @@ import java.util.Queue;
 import java.util.Set;
 
 public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
-    private Map<Q, Queue<T>> map;
+    private Map<Q, Queue<T>> map;  
 
-    
-
-    public MultiQueueImpl(Map<Q, Queue<T>> map) {
+    public MultiQueueImpl() {
         map = new HashMap<>();
     }
 
@@ -65,20 +64,40 @@ public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
 
     @Override
     public Set<T> allEnqueuedElements() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'allEnqueuedElements'");
+        Set<T> enqueuedElements = new HashSet<>();
+        for(Q queue : map.keySet()){
+            enqueuedElements.addAll(map.get(queue));
+        }
+        return enqueuedElements;
     }
 
     @Override
-    public List<T> dequeueAllFromQueue(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'dequeueAllFromQueue'");
+    public List<T> dequeueAllFromQueue(Q queue) throws IllegalArgumentException{
+       List<T> dequeueAll = new LinkedList<>();
+       if(!map.containsKey(queue)){
+            throw new IllegalArgumentException();
+        }
+       dequeueAll.addAll(map.get(queue));
+       map.get(queue).clear();
+       return dequeueAll;
     }
 
     @Override
     public void closeQueueAndReallocate(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'closeQueueAndReallocate'");
-    }
+        if(!map.containsKey(queue)){
+            throw new IllegalArgumentException();
+        }
 
+        if(map.keySet().size() == 1){
+            throw new IllegalStateException();
+        }
+
+        for(Q queues : map.keySet()){
+            if(!queues.equals(queue)){
+                map.get(queues).addAll(dequeueAllFromQueue(queue));
+                break;
+            }
+        }
+        map.remove(queue);
+    }
 }
